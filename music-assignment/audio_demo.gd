85a276f33757dd56c348a3a1e7ebb0ad44985d1c
amy <C24399391@myTUDublin.ie>
2024-12-06 14:43:07 +0000

    audio fixed

--- music-assignment/audio_demo.gd
+++ music-assignment/audio_demo.gd
@@ -17,125 +17,122 @@ func _process(delta: float) -> void:
 
 
 func _on_c_4_pressed() -> void:
-	$"c4/c4 sound".play()
+	$"Control/c4/c4 sound".play()
 	pass # Replace with function body.
 
 
 func _on_d_4_pressed() -> void:
-	$"d4/d4 sound".play()
+	$"Control/d4/d4 sound".play()
 	pass # Replace with function body.
-	
 
 
 func _on_e_4_pressed() -> void:
-	$"e4/e4 sound".play()
+	$"Control/e4/e4 sound".play()
 	pass # Replace with function body.
 	
+	
 func _on_f_4_pressed() -> void:
-	$"f4/f4 sound".play()
+	$"Control/f4/f4 sound".play()
 	pass # Replace with function body.
 
 
 func _on_g_4_pressed() -> void:
-	$"g4/g4 sound".play()
+	$"Control/g4/g4 sound".play()
 	pass # Replace with function body.
 
 
 func _on_a_4_pressed() -> void:
-	$"a4/a4 sound".play()
+	$"Control/a4/a4 sound".play()
 	pass # Replace with function body.
 
 
 func _on_b_4_pressed() -> void:
-	$"b4/b4 sound".play()
+	$"Control/b4/b4 sound".play()
 	pass # Replace with function body.
-
 
 
 func _on_c_3_pressed() -> void:
-	$"c3/c3 sound".play()
+	$"Control/c3/c3 sound".play()
 	pass # Replace with function body.
 	
 
-
 func _on_d_3_pressed() -> void:
-	$"d3/d3 sound".play()
+	$"Control/d3/d3 sound".play()
 	pass # Replace with function body.
 
 
 func _on_e_3_pressed() -> void:
-	$"e3/e3 sound".play()
+	$"Control/e3/e3 sound".play()
 	pass # Replace with function body.
 
 
 func _on_f_3_pressed() -> void:
-	$"f3/f3 sound".play()
+	$"Control/f3/f3 sound".play()
 	pass # Replace with function body.
 	
 	
 func _on_g_3_pressed() -> void:
-	$"g3/g3 sound".play()
+	$"Control/g3/g3 sound".play()
 	pass # Replace with function body.
 
 
 func _on_a_3_pressed() -> void:
-	$"a3/a3 sound".play()
+	$"Control/a3/a3 sound".play()
 	pass # Replace with function body.
 
 
 func _on_b_3_pressed() -> void:
-	$"b3/b3 sound".play()
+	$"Control/b3/b3 sound".play()
 	pass # Replace with function body.
 	
 
 func _on_c_44_pressed() -> void:
-	$"c4#4/c4# sound".play()
+	$"Control2/c4#4/c4# sound".play()
 	pass # Replace with function body.
 
 
 func _on_d_44_pressed() -> void:
-	$"d4#4/d4# sound".play()
+	$"Control2/d4#4/d4# sound".play()
 	pass # Replace with function body.
 
 
 func _on_f_44_pressed() -> void:
-	$"f4#4/f4# sound".play()
+	$"Control2/f4#4/f4# sound".play()
 	pass # Replace with function body.
 	
 
-
 func _on_g_44_pressed() -> void:
-	$"g4#4/g4# sound".play()
+	$"Control2/g4#4/g4# sound".play()
 	pass # Replace with function body.
 
 
 func _on_a_44_pressed() -> void:
-	$"a4#4/a4# sound".play()
+	$"Control2/a4#4/a4# sound".play()
 	pass # Replace with function body.
 
 
 func _on_c_33_pressed() -> void:
-	$"c3#3/c3# sound".play()
+	$"Control2/c3#3/c3# sound".play()
 	pass # Replace with function body.
 
 
 func _on_f_33_pressed() -> void:
-	$"f3#3/f3# sound".play()
+	$"Control2/f3#3/f3# sound".play()
 	pass # Replace with function body.
 
 
 func _on_g_33_pressed() -> void:
-	$"g3#3/g3# sound".play()
+	$"Control2/g3#3/g3# sound".play()
 	pass # Replace with function body.
 
 
 func _on_a_33_pressed() -> void:
-	$"a3#3/a3# sound".play()
+	$"Control2/a3#3/a3# sound".play()
 	pass # Replace with function body.
 
 
 func _on_d_33_pressed() -> void:
-	$"d3#3/d3# sound".play()
+	$"Control2/d3#3/d3# sound".play()
 	pass # Replace with function body.
 	
 
@@ -146,4 +143,13 @@ func _on_h_slider_value_changed(value: float) -> void:
 	
 	#if !toggled_on:
 		#effect.eff = 0
+	pass # Replace with function body.
+
+
+#func _on_control_gui_input(event: InputEvent) -> void:
+	
+	pass # Replace with function body.
+
+
+#func _on_control_2_gui_input(event: InputEvent) -> void:
 	pass # Replace with function body.
